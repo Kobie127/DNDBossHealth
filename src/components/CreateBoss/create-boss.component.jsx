dcb17076fedeeca2cc8d86eacce4cb09757dc7e9
Kobie Arndt <kobiearndt@Kobies-MacBook-Pro.local>
2022-04-16 21:13:19 -0400

    Added components to input conditions

--- src/components/CreateBoss/create-boss.component.jsx
+++ src/components/CreateBoss/create-boss.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import EnemyDataService from "../../services/EnemyService";
 
@@ -12,12 +13,32 @@ const CreateBoss = () => {
     }
 
     const [boss, setBoss] = useState(initialBossState);
+    const [resistance, setResistance] = useState('');
+    const [immunity, setImmunity] = useState('');
+    const [vulnerability, setVulnerability] = useState('');
+    const [bossId, setBossId] = useState(0);
     const [submitted, setSubmitted] = useState(false);
+
+    useEffect(() => {
+        console.log(resistance)
+    }, [resistance]);
 
     const handleInputChange = (event) => {
         const { name, value} = event.target;
         setBoss({...boss, [name]: value});
     };
+
+    const handleResistanceChange = (event) => {
+        setResistance(event.target.value);
+    }
+
+    const handleVulnerabilityChange = (event) => {
+        setVulnerability(event.target.value);
+    }
+
+    const handleImmunityChange = (event) => {
+        setImmunity(event.target.value);
+    }
 
     const saveBoss = () => {
         var data = {
@@ -33,13 +54,60 @@ const CreateBoss = () => {
                     description: response.data.description,
                     health: response.data.health
                 });
+                setBossId(response.data.id);
                 setSubmitted(true);
                 console.log(response.data);
+                
             })
             .catch(e => {
                 console.log(e);
             });
     };
+
+    const saveConditios = () => {
+        var resistanceData = {
+          name: resistance
+        }
+
+        var vulnerabilityData = {
+          name: vulnerability
+        }
+
+        var immunityData = {
+          name: immunity
+        }
+
+        EnemyDataService.createResistance(resistanceData, bossId)
+          .then(response => {
+            setResistance(
+              response.data.name
+            );
+          })
+          .catch(e => {
+            console.log(e);
+          });
+        
+        EnemyDataService.createVulnerability(vulnerabilityData, bossId)
+          .then(response => {
+            setVulnerability(
+              response.data.name
+            );
+          })
+          .catch(e => {
+            console.log(e);
+          });
+
+          EnemyDataService.createImmunity(immunityData, bossId)
+            .then(response => {
+              setImmunity(
+                response.data.name
+              );
+            })
+            .catch(e => {
+              console.log(e);
+            });
+    };
+
     
     const newBoss = () => {
         setBoss(initialBossState);
@@ -52,9 +120,35 @@ const CreateBoss = () => {
         <div className="submit-form">
         {submitted ? (
           <div>
-            <h4>You submitted successfully!</h4>
+            <label>Select Resistances
+                <select value={resistance} onChange={handleResistanceChange}>
+                    <option value="slashing">Slashing</option>
+                    <option value="bludgeoning">Bludgeoning</option>
+                    <option value="lightning">Lightning</option>
+                    <option value="piercing">Piercing</option>
+                </select>
+            </label>
+            <label>Select Vulnerabilities
+                <select value={vulnerability} onChange={handleVulnerabilityChange}>
+                    <option value="slashing">Slashing</option>
+                    <option value="bludgeoning">Bludgeoning</option>
+                    <option value="lightning">Lightning</option>
+                    <option value="piercing">Piercing</option>
+                </select>
+            </label>
+            <label>Select Immunities 
+                <select value={immunity} onChange={handleImmunityChange}>
+                    <option value="slashing">Slashing</option>
+                    <option value="bludgeoning">Bludgeoning</option>
+                    <option value="lightning">Lightning</option>
+                    <option value="piercing">Piercing</option>
+                </select>
+            </label>
+            <button className="btn btn-success" onClick={saveConditios}>
+              Submit Conditions
+            </button>
             <button className="btn btn-success" onClick={newBoss}>
-              Add
+              Create a new boss
             </button>
           </div>
         ) : (
--- src/services/EnemyService.js
+++ src/services/EnemyService.js
@@ -4,13 +4,28 @@ const getAll = () => {
     return http.get("/enemy");
 }
 
-const create = data => {
+const create = (data) => {
     return http.post("/enemy", data);
+}
+
+const createResistance = (data, enemyId) => {
+    return http.post(`/enemy/resistance/${enemyId}`, data);
+}
+
+const createImmunity = (data, enemyId) => {
+    return http.post(`/enemy/immunity/${enemyId}`, data);
+}
+
+const createVulnerability = (data, enemyId) => {
+    return http.post(`/enemy/vulnerability/${enemyId}`, data);
 }
 
 const EnemyService = {
     getAll,
-    create
+    create,
+    createResistance,
+    createImmunity,
+    createVulnerability
 };
 
 export default EnemyService;
